232a59783eee9b1c5b9345544562ae7809f3687b
bearflare20 <71741919+bearflare20@users.noreply.github.com>
2023-12-17 12:06:02 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+let isBinaryMode = false;
+
+function convert() {
+    const inputData = document.getElementById('inputData').value;
+    let endpoint = 'https://binary-ascii-converter.glitch.me/text-to-binary'; // Endpoint for text-to-binary conversion
+
+    if (isBinaryMode) {
+        endpoint = 'https://binary-ascii-converter.glitch.me/binary-to-ascii'; // Endpoint for binary-to-text conversion
+    }
+
+    fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data: inputData })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const result = isBinaryMode ? data.ascii_text : data.binary_text;
+        document.getElementById('output').innerText = result || data.error || 'Conversion failed';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('output').innerText = 'Error occurred';
+    });
+}
+
+function swapConversion() {
+    isBinaryMode = !isBinaryMode;
+
+    const inputPlaceholder = isBinaryMode ? 'Enter binary...' : 'Enter text...';
+    document.getElementById('inputData').placeholder = inputPlaceholder;
+
+    const convertButton = document.getElementsByTagName('button')[0];
+    convertButton.innerText = isBinaryMode ? 'Convert to Text' : 'Convert to Binary';
+
+    document.getElementById('output').innerText = ''; // Clear output when swapping
+}
